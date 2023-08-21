b741c62fd74d8c9321f5d8efa54dbede727853a3
sjvolesky <131320763+sjvolesky@users.noreply.github.com>
2023-08-21 11:06:40 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Samantha Volesky and is{" "}
+          <a
+            href="https://github.com/sjvolesky/weather-react-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
 }
