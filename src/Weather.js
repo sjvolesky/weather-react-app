c10df65d7e8cf7fe84838119c8a73b5506590239
sjvolesky <131320763+sjvolesky@users.noreply.github.com>
2023-08-22 12:56:07 -0600

    added space

--- src/Weather.js
+++ src/Weather.js
@@ -30,16 +30,13 @@ export default function Weather() {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Mostly Cloudy"
-            />
-            <div className="float-left">
-              <span className="temperature">6</span>
-              <span className="unit">°C</span>
-            </div>
-          </div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
